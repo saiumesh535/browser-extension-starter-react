0cca24deaabdf723600e20884e0f7e4de7b6d68b
saiumesh535 <d.sai535@gmail.com>
2020-10-30 20:41:12 +0530

    first commit

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+const config = {
+    entry: {
+        contentscript: path.join(process.cwd(), 'src', 'contentscript', 'index.ts'),
+        background: path.join(process.cwd(), 'src', 'background', 'index.ts'),
+        popup: path.join(process.cwd(), 'src', 'popup', 'index.tsx')
+    },
+    output: {
+        path: path.join(process.cwd(), 'dist', 'dev'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+            '.tsx',
+            '.ts'
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(process.cwd(), 'config', 'extension'),
+                    to: path.resolve(process.cwd(), 'dist', 'dev')
+                }
+            ],
+        })
+    ]
+}
+
+module.exports = config;
---
+++ src/popup/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('./index.scss');
+
+
+function App() {
+    return (
+        <div className="main">
+            <p>Hello</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
