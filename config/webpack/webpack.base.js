26ce08ef25258bba16132915961af9760bbb6ab4
saiumesh535 <d.sai535@gmail.com>
2020-10-31 13:20:53 +0530

    added prod webpack

--- config/webpack/webpack.base.js
+++ config/webpack/webpack.base.js
@@ -3,14 +3,16 @@ const path = require('path');
 
 const CopyPlugin = require('copy-webpack-plugin');
 
-const config = {
+const bundlePath = process.env.NODE_ENV || 'dev';
+
+const webpackBase = {
     entry: {
         contentscript: path.join(process.cwd(), 'src', 'contentscript', 'index.ts'),
         background: path.join(process.cwd(), 'src', 'background', 'index.ts'),
         popup: path.join(process.cwd(), 'src', 'popup', 'index.tsx')
     },
     output: {
-        path: path.join(process.cwd(), 'dist', 'dev'),
+        path: path.join(process.cwd(), 'dist', bundlePath),
         filename: '[name].js'
     },
     module: {
@@ -50,11 +52,11 @@ const config = {
             patterns: [
                 {
                     from: path.resolve(process.cwd(), 'config', 'extension'),
-                    to: path.resolve(process.cwd(), 'dist', 'dev')
+                    to: path.resolve(process.cwd(), 'dist', bundlePath)
                 }
             ],
         })
     ]
 }
 
-module.exports = config;
+module.exports = webpackBase;
