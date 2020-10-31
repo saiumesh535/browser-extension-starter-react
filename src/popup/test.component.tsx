e02c03021193f69b1ded5c79c1b6cac7eb289943
saiumesh535 <d.sai535@gmail.com>
2020-10-31 13:41:37 +0530

    added jest and testing library

---
+++ src/popup/test.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './indes.scss';
+
+function TestComponent() {
+    return (
+        <p data-testid="custom-element">Hello</p>
+    )
+}
+
+export default TestComponent;
